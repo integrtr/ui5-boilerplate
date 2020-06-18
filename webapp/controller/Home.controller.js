e58f5d7a79d0295eecfae2a2b104e86fde9d108d
TheVivekGowda <TheVivekGowda@gmail.com>
2020-06-18 20:18:45 +0530

    fix: fixed typo with npm script

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,5 +1,7 @@
 sap.ui.define(['sap/ui/core/mvc/Controller'], function (Controller) {
   'use strict';
 
-  return Controller.extend('ui5boilerplate.controller.Home', {});
+  return Controller.extend('ui5boilerplate.controller.Home', {
+    onInit: function () {},
+  });
 });
