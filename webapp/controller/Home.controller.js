fc586c9a8dd3e40ed13fd7434fd79281cc711252
Nitish Mehta <nitishmehta.nm@gmail.com>
2020-07-17 18:08:59 +0530

    feat: improve starter page

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -3,5 +3,19 @@ sap.ui.define(['sap/ui/core/mvc/Controller'], function (Controller) {
 
   return Controller.extend('ui5boilerplate.controller.Home', {
     onInit: function () {},
+
+    /**
+     * Press event handler for INTEGRTR logo
+     */
+    integrtrLogoPress: function () {
+      window.open('https://www.integrtr.com', '_blank');
+    },
+
+    /**
+     * Press event handler for GitHub logo
+     */
+    githubLogoPress: function () {
+      window.open('https://github.com/integrtr/ui5-boilerplate', '_blank');
+    },
   });
 });
