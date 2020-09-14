4389759e8d6ade1f2e3f05584e7712cb343ace5b
TheVivekGowda <TheVivekGowda@gmail.com>
2020-09-14 10:24:38 +0530

    feat: mock server setup for development & testing

---
+++ webapp/localService/mockserver.js
@@ -0,0 +1,22 @@
+sap.ui.define(['sap/ui/core/util/MockServer'], function (MockServer) {
+  'use strict';
+  return {
+    init: function () {
+      // create
+      var oMockServer = new MockServer({
+        rootUri: '', // rootUri and manifest dataSource uri should be same
+      });
+      var oUriParameters = jQuery.sap.getUriParameters();
+      // configure
+      MockServer.config({
+        autoRespond: true,
+        autoRespondAfter: oUriParameters.get('serverDelay') || 1000,
+      });
+      // simulate
+      var sPath = jQuery.sap.getModulePath('ui5boilerplate.localService');
+      oMockServer.simulate(sPath + '/metadata.xml', sPath + '/mockdata');
+      // start
+      oMockServer.start();
+    },
+  };
+});
